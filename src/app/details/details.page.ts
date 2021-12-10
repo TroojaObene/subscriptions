c63347e31ab9c9b8f6870696d9ac9284f8fac689
TroojaObene <toomasulmas@gmail.com>
2021-12-10 18:06:20 +0200

    test out new ways to display the details

--- src/app/details/details.page.ts
+++ src/app/details/details.page.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, Params } from '@angular/router';
 import { RemindersService, Reminders } from 'src/app/services/Reminders.service';
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument, DocumentReference } from '@angular/fire/compat/firestore';
+import { map } from 'rxjs/operators';
 
 
 @Component({
@@ -10,20 +11,25 @@ import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument,
   styleUrls: ['./details.page.scss'],
 })
 export class DetailsPage implements OnInit {
-  remindersCollection: any;
   reminder: any;
+  name: any;
   constructor(private route: ActivatedRoute, private remindersService: RemindersService,) {
     //this.route.params.subscribe(id => console.log(id));
+    //this.route.params.subscribe(id => this.remindersService.askReminder(id));
+    //var id = route.params(id => console.log(id));
+    //this.remindersService.askReminder(id);
+    //console.log(this.reminder);
     //this.route.params.subscribe(id => console.log(this.remindersService.askReminder(id)));
     //console.log(this.remindersService.askReminder(id));
   }
 
   ngOnInit() {
-
+    this.route.queryParams.subscribe(params => {
+      this.name = params['id'];
+    });
+    this.remindersService.askReminder(this.name);
+    console.log(this.reminder);
   }
 
-}
-function id(id: any): any {
-  throw new Error('Function not implemented.');
 }
 
