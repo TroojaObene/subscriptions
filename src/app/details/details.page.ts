a7e38203265842ea50787f6b51fd0fc7fb058563
TroojaObene <toomasulmas@gmail.com>
2021-12-11 22:50:09 +0200

    clean up the param getter in the details page

--- src/app/details/details.page.ts
+++ src/app/details/details.page.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Params } from '@angular/router';
 import { RemindersService, Reminders } from 'src/app/services/Reminders.service';
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument, DocumentReference } from '@angular/fire/compat/firestore';
 import { map } from 'rxjs/operators';
+import { identifierModuleUrl } from '@angular/compiler';
 
 
 @Component({
@@ -14,21 +15,10 @@ export class DetailsPage implements OnInit {
   reminder: any;
   name: any;
   constructor(private route: ActivatedRoute, private remindersService: RemindersService,) {
-    //this.route.params.subscribe(id => console.log(id));
-    //this.route.params.subscribe(id => this.remindersService.askReminder(id));
-    //var id = route.params(id => console.log(id));
-    //this.remindersService.askReminder(id);
-    //console.log(this.reminder);
-    //this.route.params.subscribe(id => console.log(this.remindersService.askReminder(id)));
-    //console.log(this.remindersService.askReminder(id));
   }
 
   ngOnInit() {
-    this.route.queryParams.subscribe(params => {
-      this.name = params['id'];
-    });
-    this.remindersService.askReminder(this.name);
-    console.log(this.reminder);
+    this.route.params.subscribe(id => { console.log(id.id) });
   }
 
 }
