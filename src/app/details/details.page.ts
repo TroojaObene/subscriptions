620f0ade774bb99fa4b5bed7ef161a2a0eda1a33
TroojaObene <toomasulmas@gmail.com>
2021-12-11 22:57:44 +0200

    remove unused imports

--- src/app/details/details.page.ts
+++ src/app/details/details.page.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { RemindersService, Reminders } from 'src/app/services/Reminders.service';
-import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument, DocumentReference } from '@angular/fire/compat/firestore';
-import { map } from 'rxjs/operators';
-import { identifierModuleUrl } from '@angular/compiler';
 
 
 @Component({
