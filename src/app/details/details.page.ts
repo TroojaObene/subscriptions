900cf7eb651bfd69a0d8355811276d24c9bab065
TroojaObene <toomasulmas@gmail.com>
2021-12-12 17:31:39 +0200

    temporarily set a hardcoded id param

--- src/app/details/details.page.ts
+++ src/app/details/details.page.ts
@@ -1,7 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
-import { RemindersService, Reminders } from 'src/app/services/Reminders.service';
+import { RemindersService } from 'src/app/services/Reminders.service';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument, DocumentReference } from '@angular/fire/compat/firestore';
+import { map, take, timestamp } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
+export interface Reminders {
+  name: string,
+  type: string,
+  start_date: any,
+  payment_date: number,
+  payment_frequency: number,
+  end_date: number
+}
 
 @Component({
   selector: 'app-details',
@@ -9,15 +20,28 @@ import { RemindersService, Reminders } from 'src/app/services/Reminders.service'
   styleUrls: ['./details.page.scss'],
 })
 export class DetailsPage implements OnInit {
-  reminder: any;
-  name: any;
-  constructor(private route: ActivatedRoute, private remindersService: RemindersService,) {
+  reminder: Observable<Reminders[]>;
+  remindersCollection: AngularFirestoreCollection<Reminders>;
+  param: any;
+  constructor(private route: ActivatedRoute, private afs: AngularFirestore) {
+    this.route.params.forEach(param => this.param = param['id'])
+    this.remindersCollection = this.afs.collection<Reminders>('Reminders', ref => ref.where('id', '==', 'sy2nXTZODujeZ5MLFVLd'));
+    this.reminder = this.remindersCollection.snapshotChanges().pipe(
+      map(actions => {
+        return actions.map(a => {
+          const data = a.payload.doc.data();
+          const id = a.payload.doc.id;
+          return { id, ...data }
+        });
+      })
+    );
   }
 
+  askReminders(): Observable<Reminders[]> { return this.reminder; }
+
   ngOnInit() {
-    this.remindersService.askReminder(this.route.params.subscribe(id => { return id.id }))
-    this.route.params.subscribe(id => { return id.id });
-    //this.route.params.subscribe(id => { console.log(id.id) });
+    console.log(this.askReminders())
+    console.log(this.route.params.subscribe(id => { return id }))
   }
 
 }
