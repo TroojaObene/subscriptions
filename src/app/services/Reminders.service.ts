b0f6acc3fbb99940cb04d422f11060868ee0df27
TroojaObene <toomasulmas@gmail.com>
2021-12-25 23:22:29 +0200

    add rename function

--- src/app/services/Reminders.service.ts
+++ src/app/services/Reminders.service.ts
@@ -53,6 +53,9 @@ export class RemindersService {
   removeReminder(_id: string) {
     this.firebase.collection('Reminders').doc(_id).update({ end_date: Date() });
   };
+  nameReminder(_id: string, _name: string) {
+    this.firebase.collection('Reminders').doc(_id).update({ name: _name });
+  };
   askReminder(_id) {
     this.remindersCollection.doc(_id);
     return this.reminder;
