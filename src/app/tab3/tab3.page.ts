df485f3b93087f8370bdbbdeb6e94c54fd6c2ae8
TroojaObene <toomasulmas@gmail.com>
2021-12-25 21:10:35 +0200

    add backend to tab3

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RemindersService, Reminders } from 'src/app/services/Reminders.service';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
-
-  constructor() {}
-
+export class Tab3Page implements OnInit {
+  public reminders: Observable<Reminders[]>
+  constructor(private remindersService: RemindersService, private route: Router) { }
+  ngOnInit() {
+    this.reminders = this.remindersService.askReminders();
+  }
 }
