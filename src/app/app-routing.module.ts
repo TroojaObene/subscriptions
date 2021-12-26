c8a02f358c40016c08cd0f730be48f26a8b29784
TroojaObene <toomasulmas@gmail.com>
2021-12-26 15:27:39 +0200

    add addition adding page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,10 @@ const routes: Routes = [
   },  {
     path: 'info',
     loadChildren: () => import('./info/info.module').then( m => m.InfoPageModule)
+  },
+  {
+    path: 'add',
+    loadChildren: () => import('./add/add.module').then( m => m.AddPageModule)
   }
 
 ];
