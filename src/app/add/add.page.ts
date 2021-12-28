8e5250843dfb41c0b173ace4daff690b07cc5af7
Toomas Ulmas <toomasulmas@gmail.com>
2021-12-28 22:37:56 +0200

    addition data adding page testing

--- src/app/add/add.page.ts
+++ src/app/add/add.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AdditionsService, Additions } from 'src/app/services/Additions.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddPage implements OnInit {
 
-  constructor() { }
+  constructor(private additionsService: AdditionsService, private route: Router) { }
 
   ngOnInit() {
+  }
+
+
+
+  save(_url: string, _type: string) {
+    this.additionsService.addAdditions({
+      company: _url,
+      name: _url.toUpperCase(),
+      payment_frequency: 1,
+      type: _type
+    });
+    this.route.navigate(['/tabs/tab2/']);
   }
 
 }
