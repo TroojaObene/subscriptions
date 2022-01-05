572ed78616b13ab35147265845c3e075d08f7b8e
Toomas Ulmas <toomasulmas@gmail.com>
2022-01-05 23:48:28 +0200

    add data to addition page

--- src/app/addition/addition.page.ts
+++ src/app/addition/addition.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/compat/firestore';
+import { RemindersService } from 'src/app/services/Reminders.service';
+import { formatDate } from '@angular/common';
 
 @Component({
   selector: 'app-addition',
@@ -6,10 +10,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./addition.page.scss'],
 })
 export class AdditionPage implements OnInit {
+  Rem_id: string;
+  sub
+  data: any;
+  DocReference: AngularFirestoreDocument;
 
-  constructor() { }
 
+
+  constructor(private remindersService: RemindersService, private route: ActivatedRoute, private afs: AngularFirestore, private router: Router) { }
   ngOnInit() {
+    this.data = {}
+    this.Rem_id = this.route.snapshot.paramMap.get('id')
+    console.log("Recive", this.Rem_id)
+    this.DocReference = this.afs.doc(`Additions/${this.Rem_id}`)
+    this.sub = this.DocReference.valueChanges().subscribe(val => {
+      this.data = val
+    })
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe()
+  }
+
+  //save
+  save(a) {
+    let now = new Date();
+    let dd = String(now.getDate()).padStart(2, '0');
+    let ddnum = parseInt(dd);
+    let logo = "https://logo.clearbit.com/" + a.company
+    this.remindersService.addReminders({
+      name: a.name,
+      company: a.company,
+      logo: logo,
+      start_date: formatDate(now, 'MM-dd-yyyy', 'en-US'),
+      payment_date: ddnum,
+      payment_frequency: a.payment_frequency,
+      end_date: null,
+      cost: a.cost,
+      next_date: formatDate(now.setMonth(now.getMonth() + 1), 'MM-dd-yyyy', 'en-US')
+    });
+    this.router.navigate(['/tabs/tab1']);
   }
 
 }
