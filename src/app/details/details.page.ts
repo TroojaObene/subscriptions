4f11db8dbe3b667422b7b61e6ed3118e31b931c8
Toomas Ulmas <toomasulmas@gmail.com>
2022-01-06 21:18:33 +0200

    move stuff into async

--- src/app/details/details.page.ts
+++ src/app/details/details.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument, DocumentReference } from '@angular/fire/compat/firestore';
 import { toDate } from 'date-fns';
+import { testUserAgent } from '@ionic/core/dist/types/utils/platform';
+import { Reminders } from 'src/app/services/Reminders.service';
 
 @Component({
   selector: 'app-details',
@@ -16,10 +18,13 @@ export class DetailsPage implements OnInit {
   DocReference: AngularFirestoreDocument;
   constructor(private route: ActivatedRoute, private afs: AngularFirestore, private router: Router) { }
   ngOnInit() {
-    this.single = {}
     this.Rem_id = this.route.snapshot.paramMap.get('id')
     console.log("Recive", this.Rem_id)
-    this.DocReference = this.afs.doc(`Reminders/${this.Rem_id}`)
+    this.single = this.get_data(this.Rem_id)
+  }
+
+  async get_data(id: string) {
+    this.DocReference = this.afs.doc(`Reminders/${id}`)
     this.sub = this.DocReference.valueChanges().subscribe(val => {
       this.single = val
     })
