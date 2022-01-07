13bb7751700009dfbfa08c2b9614fbafa987d640
Toomas Ulmas <toomasulmas@gmail.com>
2022-01-07 23:54:18 +0200

    add automatic date updater

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { RemindersService, Reminders } from 'src/app/services/Reminders.service';
+import { AngularFirestore, AngularFirestoreCollection, DocumentReference } from '@angular/fire/compat/firestore';
 import { Observable } from 'rxjs';
 import { Router } from '@angular/router';
+import { format } from 'date-fns';
 
 @Component({
   selector: 'app-tab1',
@@ -10,9 +12,14 @@ import { Router } from '@angular/router';
 })
 export class Tab1Page implements OnInit {
   public reminders: Observable<Reminders[]>
-  constructor(private remindersService: RemindersService, private route: Router) { }
+  constructor(private remindersService: RemindersService, private route: Router, private firebase: AngularFirestore) { }
   ngOnInit() {
     this.reminders = this.remindersService.askReminders();
+    this.reminders.forEach(data => {
+      data.forEach(object => {
+        this.nextDate(object)
+      })
+    })
   }
 
   navInfo() {
@@ -24,4 +31,30 @@ export class Tab1Page implements OnInit {
     console.log("send", reminder)
   }
 
+  async nextDate(data) {
+    let id = data.id
+    let next_date_old = data.next_date
+    let dd_old = data.payment_date
+    let dd_now = new Date().getDay()
+    let mm_now = new Date().getMonth()
+    let yy_now = new Date().getFullYear()
+
+    let next_date_date = new Date()
+    next_date_date.setFullYear(yy_now)
+    next_date_date.setDate(dd_old)
+    if (dd_now > dd_old) {
+      next_date_date.setMonth(mm_now + 1)
+    } else {
+      next_date_date.setMonth(mm_now)
+    }
+
+    let new_next_date = format(next_date_date, 'MM-dd-yyyy')
+    //console.log("Old: ", next_date_old)
+    //console.log("New: ", new_next_date)
+
+    if (next_date_old === new_next_date) { } else {
+      this.firebase.collection('Reminders').doc(id).update({ next_date: new_next_date })
+      console.log("Update ", id, " date to ", new_next_date)
+    }
+  }
 }
