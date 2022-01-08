3329bb3c52fb8aa6741546ad8d011f95fe837fe4
Toomas Ulmas <toomasulmas@gmail.com>
2022-01-08 13:02:33 +0200

    move tab3 as info and remove it from the tab bar

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab3',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page implements OnInit {
-  constructor() { }
+  constructor(private router: Router) { }
   ngOnInit() { }
 
   Git() {
     window.open("//github.com/TroojaObene/subscriptions")
+  }
+
+  back() {
+    this.router.navigate(['/tabs/tab1/']);
   }
 }
