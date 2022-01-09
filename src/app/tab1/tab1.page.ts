74eac39cecd76d202f0a908c8bc908acb4b26f91
Toomas Ulmas <toomasulmas@gmail.com>
2022-01-09 11:56:37 +0200

    fix up a typo I made in #7d91850

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -35,10 +35,11 @@ export class Tab1Page implements OnInit {
     let id = data.id
     let next_date_old = data.next_date
     let dd_old = data.payment_date
-    let dd_now = new Date().getDay()
-    dd_now = dd_now + 2
-    let mm_now = new Date().getMonth()
-    let yy_now = new Date().getFullYear()
+    let now = new Date()
+    let dd_now = now.getDate()
+    //dd_now = dd_now + 2
+    let mm_now = now.getMonth()
+    let yy_now = now.getFullYear()
 
     //console.log("")
     //console.log(data.name, " at ", next_date_old)
